9d6e697bbcd6f88ac7c5da41412d0f50b4efbbb7
Kumkum Gupta <kumkumgupta1928@gmail.com>
2025-08-21 17:07:48 +0530

    updated code

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/users", require("./routes/user.routes"));
-app.use("/api/stores", require("./routes/store.routes"));
-app.use("/api/ratings", require("./routes/rating.routes"));
+app.use("/api/auth", require("./routes.js/auth.routes"));
+app.use("/api/users", require("./routes.js/user.routes"));
+app.use("/api/stores", require("./routes.js/store.routes"));
+app.use("/api/ratings", require("./routes.js/rating.routes"));
 
 module.exports = app;
